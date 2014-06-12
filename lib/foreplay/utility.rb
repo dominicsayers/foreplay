ec39860b1c354a4583c094e4a11e9b0c4dd94a66
Dominic Sayers <dominic@sayers.cc>
2014-06-12 16:48:40 +0100

    Rubocop code improvements

--- lib/foreplay/utility.rb
+++ lib/foreplay/utility.rb
@@ -7,15 +7,15 @@ module Foreplay
     #   h1.supermerge(h2)
     #   #=> {:x=>{:y=>[4, 5, 6, 7, 8, 9]}, :z=>[7, 8, 9, "xyz"]}
     def self.supermerge(hash, other_hash)
-      raise 'supermerge only works if you pass two hashes' unless hash.is_a?(Hash) && other_hash.is_a?(Hash)
+      fail 'supermerge only works if you pass two hashes' unless hash.is_a?(Hash) && other_hash.is_a?(Hash)
 
       new_hash = hash.deep_dup.with_indifferent_access
 
-      other_hash.each_pair do |k,v|
+      other_hash.each_pair do |k, v|
         tv = new_hash[k]
 
         if tv.is_a?(Hash) && v.is_a?(Hash)
-          new_hash[k] = Foreplay::Utility::supermerge(tv, v)
+          new_hash[k] = Foreplay::Utility.supermerge(tv, v)
         elsif tv.is_a?(Array) || v.is_a?(Array)
           new_hash[k] = Array.wrap(tv) + Array.wrap(v)
         else

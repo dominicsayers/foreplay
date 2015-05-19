3078204ba9b5a09e1098177e67dbd8706e01c9ec
Dominic Sayers <dominic@sayers.cc>
2015-05-20 00:12:46 +0100

    Fix deep_dup dependency

--- lib/foreplay.rb
+++ lib/foreplay.rb
@@ -1,3 +1,4 @@
+require 'active_support/core_ext'
 require 'foreplay/version'
 require 'foreplay/engine'
 require 'foreplay/launcher'
@@ -12,5 +13,58 @@ module Foreplay
 
   def terminate(message)
     fail message
+  end
+end
+
+require 'active_support/core_ext/object'
+
+class Hash
+  # Returns a new hash with +hash+ and +other_hash+ merged recursively, including arrays.
+  #
+  #   h1 = { x: { y: [4,5,6] }, z: [7,8,9] }
+  #   h2 = { x: { y: [7,8,9] }, z: 'xyz' }
+  #   h1.supermerge(h2)
+  #   #=> {:x=>{:y=>[4, 5, 6, 7, 8, 9]}, :z=>[7, 8, 9, "xyz"]}
+  def supermerge(other_hash)
+    fail 'supermerge only works if you pass a hash. '\
+      "You passed a #{self.class} and a #{other_hash.class}." unless other_hash.is_a?(Hash)
+
+    new_hash = deep_dup
+
+    other_hash.each_pair do |k, v|
+      tv = new_hash[k]
+
+      if tv.is_a?(Hash) && v.is_a?(Hash)
+        new_hash[k] = tv.supermerge(v)
+      elsif tv.is_a?(Array) || v.is_a?(Array)
+        new_hash[k] = Array.wrap(tv) + Array.wrap(v)
+      else
+        new_hash[k] = v
+      end
+    end
+
+    new_hash
+  end
+end
+
+# Some useful additions to the String class
+class String
+  colors = %w(black red green yellow blue magenta cyan white)
+
+  colors.each_with_index do |fg_color, i|
+    fg = 30 + i
+    define_method(fg_color) { ansi_attributes(fg) }
+
+    colors.each_with_index do |bg_color, j|
+      define_method("#{fg_color}_on_#{bg_color}") { ansi_attributes(fg, 40 + j) }
+    end
+  end
+
+  def ansi_attributes(*args)
+    "\e[#{args.join(';')}m#{self}\e[0m"
+  end
+
+  def fake_erb
+    gsub(/(<%=\s+([^%]+)\s+%>)/) { |e| eval "_ = #{e.split[1]}" }
   end
 end
--- spec/lib/hash_spec.rb
+++ spec/lib/hash_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require 'hash'
+require 'foreplay'
 
 describe Hash do
   context '#supermerge' do

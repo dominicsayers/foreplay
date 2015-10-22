b760c6465f39e862249740cd4338a12c0d43e4e6
Dominic Sayers <dominic@sayers.cc>
2015-10-22 23:15:20 +0100

    Investigate YAML differences

--- spec/lib/yaml_spec.rb
+++ spec/lib/yaml_spec.rb
@@ -1,11 +1,15 @@
 describe YAML do
   context '#escape' do
+    it 'uses the expected YAML version' do
+      expect(YAML::VERSION).to eq '2.0.8'
+    end
+
     it 'correctly escape a basic string' do
       expect(YAML.escape 'brian').to eq('brian')
     end
 
     it 'correctly escape a troublesome string' do
-      expect(YAML.escape '{{moustache}}').to eq('"{{moustache}}"')
+      expect(YAML.escape '{{moustache}} beard').to eq('"{{moustache}} beard"')
       expect(YAML.escape "Brian O'Brien").to eq("Brian O'Brien")
     end
   end
